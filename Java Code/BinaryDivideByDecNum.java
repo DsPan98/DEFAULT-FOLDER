51fa8503593d22f548e77b88e1f272e05a4b9ca2
DsPan98 <60277682+DsPan98@users.noreply.github.com>
2020-03-02 21:00:34 -0500

    Update BinaryDivideByDecNum.java

--- Java Code/BinaryDivideByDecNum.java
+++ Java Code/BinaryDivideByDecNum.java
@@ -3,6 +3,15 @@ package LC;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * Given an array A with only 0 and 1s, representing binary numbers;
+ * the i-th subarray representing a binary number constituted by
+ * all numbers from A[0] to A[i].
+ * Another integer is given, in decimal, as the divisor (number used to divide)
+ * An boolean array list would be returned, recording if the binary number at i 
+ * could be divided.
+ */
+
 public class BinaryDivideByDecNum {
 	public List<Boolean> prefixesDivBy5(int[] A, int DecDiv) {
     	List<Boolean> forReturnList =new ArrayList<>(A.length);
